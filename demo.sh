8fbf18f5f32400cfafedc94fe9e12b6ef7536da9
ianmaddocks <ian@maddocks.name>
2022-05-05 21:18:52 +0100

    added dont validate tls

--- demo.sh
+++ demo.sh
@@ -7,4 +7,4 @@ terraform apply -auto-approve
 
 sleep 5
 printf "\nYou should see '/version' as a reponse below:\n"
-curl https://microservice2.maddocks.name/version
+curl -k https://microservice2.maddocks.name/version
